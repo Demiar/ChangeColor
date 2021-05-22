39b70e1e0ab2df07a764406f97c76c832bca8081
BelyaevAlexey <alexey174@gmail.com>
2021-05-22 10:03:12 +0300

    make storyboard add code

--- ChangeColor/ViewController.swift
+++ ChangeColor/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redValue: UILabel!
+    @IBOutlet weak var greenValue: UILabel!
+    @IBOutlet weak var blueValue: UILabel!
+    
+    var red: CGFloat = 0.5
+    var green: CGFloat = 0.5
+    var blue: CGFloat = 0.5
+    
+    @IBAction func addRedColor(_ sender: UISlider) {
+        sender.minimumTrackTintColor = UIColor.red
+        red = CGFloat(sender.value)
+        redValue.text = String(format: "%.2f", sender.value)
+        displayColors()
     }
-
-
+    
+    @IBAction func addGreenColor(_ sender: UISlider) {
+        sender.minimumTrackTintColor = UIColor.green
+        green = CGFloat(sender.value)
+        greenValue.text = String(format: "%.2f", sender.value)
+        displayColors()
+    }
+    
+    @IBAction func addBlueColor(_ sender: UISlider) {
+        sender.minimumTrackTintColor = UIColor.blue
+        blue = CGFloat(sender.value)
+        blueValue.text = String(format: "%.2f", sender.value)
+        displayColors()
+    }
+    
+    func displayColors() {
+        colorView.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+    }
 }
 
